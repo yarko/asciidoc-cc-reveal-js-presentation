063d51e2de5ec7f73c26131c9167ee32ac241ce4
Yarko Tymciurak <yarkot1@gmail.com>
2017-03-10 16:45:06 -0600

    make sure a bin dir exists;

--- init_deps.sh
+++ init_deps.sh
@@ -4,5 +4,6 @@ cd lib
 git clone https://github.com/salyh/asciidoctor-reveal.js
 git clone https://github.com/salyh/reveal.js
 git clone --depth 1 https://github.com/astefanutti/decktape.git
+[ -d decktape/bin ] || mkdir -p decktape/bin
 curl -L https://github.com/astefanutti/decktape/releases/download/v1.0.0/phantomjs-linux-x86-64 -o decktape/bin/phantomjs
 chmod +x decktape/bin/phantomjs
